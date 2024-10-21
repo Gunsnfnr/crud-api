ce08678ca90b617be0d8995eacd8bc2df6b1a95d
Gunsnfnr <a.j.p.010203@gmail.com>
2024-10-21 23:51:20 +0300

    refactor: add internal server error handling

--- src/index.ts
+++ src/index.ts
@@ -9,26 +9,32 @@ const host = 'localhost';
 const port = Number(process.env.PORT);
 
 const requestListener = function (req: IncomingMessage, res: ServerResponse) {
-  switch (req.method) {
-    case 'GET':
-      handleGet(req, res);
-      break;
-    case 'POST':
-      handlePost(req, res);
-      break;
-    case 'PUT':
-      handlePut(req, res);
-      break;
-    case 'DELETE':
-      handleDelete(req, res);
-      break;
-    default:
-      res.writeHead(405);
-      res.end('Method Not Allowed. Server is able to handle GET, POST, PUT, DELETE requests.');
+  try {
+    switch (req.method) {
+      case 'GET':
+        handleGet(req, res);
+        break;
+      case 'POST':
+        handlePost(req, res);
+        break;
+      case 'PUT':
+        handlePut(req, res);
+        break;
+      case 'DELETE':
+        handleDelete(req, res);
+        break;
+      default:
+        res.writeHead(405);
+        res.end('Method Not Allowed. Server is able to handle GET, POST, PUT, DELETE requests.');
+    }
+  } catch (err) {
+    res.writeHead(500);
+    res.end('Internal Server Error');
   }
 };
 
 const server = http.createServer(requestListener);
+
 server.listen(port, host, () => {
   console.log(`Server is running on http://${host}:${port}`);
 });
